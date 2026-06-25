3c7aa4e3054307d21d20fbc9c41ea055c844e4e8
Panagiotis Grigorakis <p.grigorakis@gmail.com>
2026-06-26 00:32:25 +0200

    Added function to get href and img src from html body and their tests

--- parse_html.go
+++ parse_html.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/url"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
@@ -37,4 +39,54 @@ func getFirstParagraphFromHTML(html string) string {
 
 	return strings.TrimSpace(paragraphText)
 
+}
+
+func getURLsFromHTML(htmlBody string, baseURL *url.URL) ([]string, error) {
+	var urls []string
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(htmlBody))
+	if err != nil {
+		return nil, err
+	}
+
+	doc.Find("a[href]").Each(func(_ int, s *goquery.Selection) {
+		href, ok := s.Attr("href")
+		if !ok || strings.TrimSpace(href) == "" {
+			return
+		}
+
+		url, err := url.Parse(href)
+		if err != nil {
+			fmt.Printf("could not parse href %v: %v", href, err)
+			return
+		}
+
+		urls = append(urls, baseURL.ResolveReference(url).String())
+	})
+
+	return urls, err
+}
+
+func getImagesFromHTML(htmlBody string, baseURL *url.URL) ([]string, error) {
+	var imageURLs []string
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(htmlBody))
+	if err != nil {
+		return []string{}, err
+	}
+
+	doc.Find("img").Each(func(_ int, s *goquery.Selection) {
+		imgSrc, ok := s.Attr("src")
+		if !ok || strings.TrimSpace(imgSrc) == "" {
+			return
+		}
+
+		url, err := url.Parse(imgSrc)
+		if err != nil {
+			fmt.Printf("could not parse img src %v: %v", imgSrc, err)
+			return
+		}
+
+		imageURLs = append(imageURLs, baseURL.ResolveReference(url).String())
+	})
+
+	return imageURLs, err
 }
--- parse_html_test.go
+++ parse_html_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"net/url"
+	"reflect"
+	"testing"
+)
 
 func TestGetHeadingFromHTMLBasic(t *testing.T) {
 	tests := []struct {
@@ -88,6 +92,125 @@ func TestGetFirstParagraphFromHTMLMainPriority(t *testing.T) {
 			actual := getFirstParagraphFromHTML(tc.inputBody)
 			if actual != tc.expected {
 				t.Errorf("Test %v - %s FAIL: expected URL: %v, actual: %v", i, tc.name, tc.expected, actual)
+			}
+		})
+	}
+}
+
+func TestGetURLsFromHTMLAbsolute(t *testing.T) {
+	tests := []struct {
+		name      string
+		inputURL  string
+		inputBody string
+		expected  []string
+	}{
+		{
+			name:      "valid HTML",
+			inputURL:  "https://crawler-test.com",
+			inputBody: `<html><body><a href="https://crawler-test.com"><span>Boot.dev</span></a></body></html>`,
+			expected:  []string{"https://crawler-test.com"},
+		},
+		{
+			name:      "empty HTML",
+			inputURL:  "https://crawler-test.com",
+			inputBody: "",
+			expected:  nil,
+		},
+		{
+			name:      "malformed HTML",
+			inputURL:  "https://crawler-test.com",
+			inputBody: `<html><body><a f="https://crawler-test.com"><span>Boot.dev</span></a></body></html>`,
+			expected:  nil,
+		},
+		{
+			name:     "multiple links ",
+			inputURL: "https://crawler-test.com",
+			inputBody: `<html><body><a href="https://crawler-test.com"><span>Boot.dev</span></a>
+			            <a href="https://crawler-test1.com"><span>Boot.dev</span></a>
+			            <a href="https://crawler-test2.com"><span>Boot.dev</span></a>
+						</body></html>`,
+			expected: []string{"https://crawler-test.com", "https://crawler-test1.com", "https://crawler-test2.com"},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			baseURL, err := url.Parse(tc.inputURL)
+			if err != nil {
+				t.Errorf("couldn't parse input URL: %v", err)
+				return
+			}
+
+			actual, err := getURLsFromHTML(tc.inputBody, baseURL)
+			if !reflect.DeepEqual(actual, tc.expected) {
+				t.Errorf("expected %v, got %v", tc.expected, actual)
+			}
+		})
+	}
+
+}
+
+func TestGetImagesFromHTMLRelative(t *testing.T) {
+	tests := []struct {
+		name      string
+		inputURL  string
+		inputBody string
+		expected  []string
+		wantErr   bool
+	}{
+		{
+			name:      "valid HTML",
+			inputURL:  "https://crawler-test.com",
+			inputBody: `<html><body><img src="/logo.png" alt="Logo"></body></html>`,
+			expected:  []string{"https://crawler-test.com/logo.png"},
+			wantErr:   false,
+		},
+		{
+			name:      "empty HTML",
+			inputURL:  "https://crawler-test.com",
+			inputBody: "",
+			expected:  nil,
+			wantErr:   false,
+		},
+		{
+			name:      "malformed HTML",
+			inputURL:  "https://crawler-test.com",
+			inputBody: `<html><body><img "/logo.png" alt="Logo"></body></html>`,
+			expected:  nil,
+			wantErr:   false,
+		},
+		{
+			name:     "multiple images HTML",
+			inputURL: "https://crawler-test.com",
+			inputBody: `<html><body><img src="/logo.png" alt="Logo">
+						<img src="/logo1.png" alt="Logo">
+						<img src="/logo2.png" alt="Logo">
+						</body></html>`,
+			expected: []string{"https://crawler-test.com/logo.png", "https://crawler-test.com/logo1.png", "https://crawler-test.com/logo2.png"},
+			wantErr:  false,
+		},
+	}
+
+	for i, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			baseURL, err := url.Parse(tc.inputURL)
+			if err != nil {
+				t.Errorf("couldn't parse input URL: %v", err)
+				return
+			}
+
+			actual, err := getImagesFromHTML(tc.inputBody, baseURL)
+			if err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+
+			if (err != nil) != tc.wantErr {
+				t.Errorf("Test %v - '%s' FAIL: unexpected error: %v, wantErr: %v", i, tc.name, err, tc.wantErr)
+				return
+			}
+
+			if !reflect.DeepEqual(actual, tc.expected) {
+				t.Errorf("expected %v, got %v", tc.expected, actual)
 			}
 		})
 	}
